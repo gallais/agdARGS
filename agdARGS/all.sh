aaa43d7ed83c94945abe93781e134d8df2452e15
Langston Barrett <langston.barrett@gmail.com>
2018-04-19 11:48:30 -0700

    instructions on building

--- agdARGS/all.sh
+++ agdARGS/all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "module agdARGS.All where\n" > All.agda
+echo "module agdARGS.All where" > All.agda
 git ls-tree --full-tree -r --name-only HEAD | grep "\.agda$" | sed "s/\.agda$//" | sed "s|^|open import |" | sed "s|/|.|g" | sort >> All.agda
